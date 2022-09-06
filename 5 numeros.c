c8d527e54768fc3c4ac65bf20763b43e812d2258
ArthurSimplicio <105257098+ArthurSimplicio@users.noreply.github.com>
2022-09-06 07:05:25 -0300

    Add files via upload

---
+++ 5 numeros.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <time.h>
+
+
+int gerAleatorio(int vetOriginal[10], int i){
+	int vetOriginal[10], i;
+				printf("%d ", vetOriginal[i]);
+	}
+}
+
+int main(){            
+	srand(time(NULL));
+	printf("     MENU\n");
+	printf("===============\n");
+	printf("Digite 1:Para criar 10 numeros aleatorios.\n");
+	printf("Digite 2:Para mostrar o maior valor\n");
+	printf("Digite 3:Para contar quantos numeros sao pares.\n");
+	printf("Digite 4:Para mostrar a soma dos elementos.\n");
+	printf("Digite 5:Para mostrar os elementos na ordem inversa.\n");
+	printf("Digite 6:Para sair.\n");
+	scanf("%d", &n);
+	
+	
+	switch(n){
+		int menor, soma, par;
+		
+		
+		
+		cas maior,e 1:
+			gerAleatorio(vetOriginal);
+		maior = menor = vetOriginal[0];	main();
+		case 2:
+			gerMaior(i,vetOriginal);
+			//main();
+			break;
+			
+		
+			for(i=0; i<10; i++){
+				if(vetOriginal[i]%2 == 0){
+		9			par = par + 1;
+			break;
+		case 4:
+			for(i=0; i<10; i++){
+				soma = soma + vetOriginal[i];
+			}
+			printf("A soma dos elementos e %d\n", soma);
+			break;
+		case 5:
+			for(i=9-1; i>=0; i--){
+				printf("%d ", vetOriginal[i]);
+			}
+			break;
+		case 6:
+			printf("Voce saiu!");
+			break;
+		default:
+			printf("ERRO!NUMERO INVALIDO!!!");
+			break;
+}	} 
